4cc98a1b8a6e565c505f7ccdf51474627f2c4b88
AdrielClaflin <44910017+AdrielClaflin@users.noreply.github.com>
2019-12-12 21:33:49 -0700

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -18,4 +18,45 @@ public class Utils : MonoBehaviour
 
         return (mats.ToArray());
     }
+
+    // ============== Hitscan/Raycast shooting function and sfx spawning =============== \\
+    public void Shoot()
+    {
+        //Reseting the laser lifetime
+        laserLife = laserTime;
+        laser.enabled = true;
+
+        //The real Raycast
+        RaycastHit hit;
+        if (Physics.Raycast(barrel.transform.position, barrel.transform.forward, out hit))
+        {
+
+            //Start Hit logic
+
+            if (hit.transform.gameObject.tag == "Thrall")
+            {
+                Thrall thrawl = hit.transform.GetComponent<Thrall>();
+                if (thrawl != null)
+                {
+                    thrawl.TakeDamage(damage);
+                }
+            }
+
+            //end hit logic
+
+            //Start SFX
+            //Starting point for the laser line
+            laser.SetPosition(0, barrel.transform.position);
+            //Ending point for the line
+            laser.SetPosition(1, hit.point);
+
+            GameObject sparkGO = Instantiate(sparkEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            Destroy(sparkGO, 2f);
+            //End SFX
+        }
+
+        //print("Shot fired");
+        coolDown = coolTime;
+    }
 }
